77099a55fe9e9210d68096d74e1104ca2dec06db
digimori <hanaorikuu@gmail.com>
2023-10-09 14:58:20 +0000

    Data types notes added.

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,32 @@
+// Value types:
+// Objects:
+let myProfile = {
+    name: 'Cee',
+    age: 31,
+    hobbies: ['Knitting', 'Painting', 'Programming'],
+    currentWork: 'Pharmacy Dispenser',
+}
+
+// Primitives: 
+let name = "Cee";
+let age = 31;
+
+//Data types
+
+// Boolean (True or False/ Yes or No answers)
+let booBoolean = true;
+
+//Strings
+let stringDataType = "This is a string";
+
+// Numbers
+let thisIsANumber = 31;
+
+// Arrays
+let thisIsAnArray = ['Seventy Three', 74, false];
+
+// Undefined, null, symbol, bigInt
+let person; // This is undefined, or "Empty value"
+// null is similar.
+// symbol is a unique and unchangeable value
+//BigInt is a larger number than what the number type can hold.
