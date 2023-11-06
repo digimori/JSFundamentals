2fd00b415315aac81f93d718facf686e031a21f3
digimori <hanaorikuu@gmail.com>
2023-11-06 14:48:14 +0000

    Arrow function notes added to scripts.

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -20,3 +20,45 @@ const orangeJuice = fruitProcessor(0, 5);
 console.log(orangeJuice);
 
 // So the function runs, taking the arguments and processing them, giving us a return statement of the variable 'juice'
+
+function calcAge1 (birthYear) {
+    const age = 2023 - birthYear;
+    return age;
+}
+
+// We can also just return it as a single statement: return 2023 - birthYear;
+
+console.log(calcAge1(1992));
+
+// Function expression - Anonymous function: (The above is declaration)
+const calcAge2 = function (birthYear) {
+    return 2023 - birthYear;
+} 
+
+const age2 = calcAge2(1992);
+console.log(age2);
+
+// Arrow functions, a function exression and is shorter than the above:
+const arrowBirthYear = birthYear => 2023 - birthYear;
+// The return is implicit, so the keyword isn't needed here.
+
+const loggedArrow = arrowBirthYear(1992);
+console.log(loggedArrow)
+
+// More complex arrow functions with one parameter, but multiple lines of code:
+const yearsUntilRetirement = birthYear => {
+    const age = 2023 - birthYear;
+    const retirement = 65 - age;
+    return retirement;
+}
+
+console.log(yearsUntilRetirement(1992));
+
+// Arrow functions with multiple parameters:
+const yearsUntilRetirement2 = (birthYear, firstName) => {
+    const age = 2023 - birthYear;
+    const retirement = 65 - age;
+    return `${firstName} will retire in ${retirement} years.`
+}
+
+console.log(yearsUntilRetirement2(1992, 'Paige'));
