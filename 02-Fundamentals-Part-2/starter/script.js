54ee3f14075f20cfb4965773c7faff93241a30b6
digimori <hanaorikuu@gmail.com>
2023-11-07 19:34:53 +0000

    For Loop exercise completed.

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -425,3 +425,24 @@ while(dice !== 6) {
 }
 
 // REMEMBER TO NOT JUST PUT THE MATH METHOD IN WITHOUT ASSIGNMENT TO VARIABLE. IT WILL INFINITE!!!
+
+// For Loop exercise:
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+  }
+  
+  /* Write your code below. Good luck! 🙂 */
+  
+  const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+  const tips = [];
+  const totals = [];
+  
+  for(let i = 0; i <= bills.length -1; i++) {
+     // const tips = calcTip(bills[i]); // Not entirely necessary as we've defined it below
+      tips.push(calcTip(bills[i]));
+      totals.push(bills[i] + tips[i]);
+  }
+  console.log(tips);
+  console.log(totals);
+
+  
