5dcea565b4bcd25bc43ee141f487adb6b883111a
digimori <hanaorikuu@gmail.com>
2023-11-12 13:05:21 +0000

    Game logic further implemented taking into account for score hitting 0.

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -13,15 +13,47 @@ document.querySelector('.number').textContent;
 // Setting/Manipulating, these will be changed using if/else when events happen:
 document.querySelector('.message').textContent = 'Correct Number!';*/
 
+const randomNum = Math.trunc(Math.random() * 20) + 1; // Number between 1 and 20;
+let initialScore = 20;
+
+let guessMessage = document.querySelector('.message').textContent;
+let highScore = document.querySelector('.highscore').textContent;
+let numBox = document.querySelector('.number').textContent;
+
 document.querySelector('.check').addEventListener('click', function () {
-  let guessNum = document.querySelector('.guess').value;
-  if (guessNum <= 0 || guessNum > 20) {
-    console.log('Please enter a number between 1 and 20 only!');
-  } else if (guessNum <= 15) {
-    console.log('Too low!');
-  } else if (guessNum >= 17) {
-    console.log('Too high!');
-  } else {
-    console.log('Just right.');
+  let guessNum = Number(document.querySelector('.guess').value);
+  let randomizer = (document.querySelector('.number').textContent = randomNum);
+
+  if (!guessNum) {
+    document.querySelector('.message').textContent =
+      'Please enter a number between 1 and 20';
+  } else if (guessNum === randomNum) {
+    document.querySelector('.number').textContent = randomNum;
+    document.querySelector('.message').textContent = 'Correct number!';
+
+    initialScore++;
+    document.querySelector('.score').textContent = initialScore;
+  } else if (guessNum < randomNum) {
+    if (initialScore > 1) {
+      document.querySelector('.number').textContent = randomNum;
+      document.querySelector('.message').textContent = 'Guess is too low!';
+      initialScore--;
+      document.querySelector('.score').textContent = initialScore;
+    } else {
+      document.querySelector('.message').textContent = 'Game Over! You lose.';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guessNum > randomNum) {
+    if (initialScore > 1) {
+      document.querySelector('.number').textContent = randomNum;
+      document.querySelector('.message').textContent = 'Guess is too high!';
+      initialScore--;
+      document.querySelector('.score').textContent = initialScore;
+    } else {
+      document.querySelector('.message').textContent = 'Game Over! You lose.';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
+
+// We need a 0 logic. Can't go lower than zero.
